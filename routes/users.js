ba684822ba05408109ef005b3953f1a8a5837c9c
TimHyatt707 <timhyatt707@gmail.com>
2017-10-27 19:55:29 -0700

    Added Controllers

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const Boom = require("Boom");
 const router = express.Router();
+const usersController = require("./../controllers/usersController");
 
-router.get("/users/:userid", (req, res));
+router.get("/users/:userid", usersController.getUserById);
+router.get("/users/:userid", usersController.getTransactionsByUser);
+router.get("/users/:userd", usersController.getCategoriesByUser);
 
-router.post("/users", (req, res) => {
-  res.json("Success");
-});
+router.post("/users", usersController.createUser);
+router.post("/users/:userid/categories", usersController.createCategory);
+router.post("/users/:userid/transactions");
 
-router.post("/users/login", (req, res) => {
-  res.json("Sucess");
-});
+router.post("/users/login", usersController.loginUser);
+
+// router.all("*", (next) => {
+//   next(Boom.Meth)
+// })
 
 module.exports = router;
